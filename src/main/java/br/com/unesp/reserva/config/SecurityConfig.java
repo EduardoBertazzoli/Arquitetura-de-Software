8f3f59e2888ea455466dad37808448377f5a3068
izasanches <izabele.sanches@hotmail.com>
2024-10-09 21:22:58 -0300

    implementation oauth2

--- src/main/java/br/com/unesp/reserva/config/SecurityConfig.java
+++ src/main/java/br/com/unesp/reserva/config/SecurityConfig.java
@@ -8,9 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -19,11 +16,19 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     
     @Bean
-    private SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws 
+            Exception {
         
-        http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
-                .csrf(csrf -> csrf.disable())
-                .oauth2ResourceServer(oauth2ResourceServerCustomizer)
+        http.authorizeHttpRequests(authorize -> 
+                authorize
+                        .requestMatchers("/", "/login**").permitAll()
+                        .anyRequest()
+                        .authenticated())                
+                .oauth2Login(oauth2 -> 
+                oauth2
+                    .loginPage("/oauth2/authorization/google")
+                    .defaultSuccessUrl("/", true)
+            );
         
         return http.build();
     }
